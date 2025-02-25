4d426d63e41bcc6284d2a6661b56e9839ab195a6
RenatoCernades0107 <renato.cernades@utec.edu.pe>
2025-02-24 22:03:37 -0500

    comentado y refactorizado: funciona el predictor y se conecta correctamente al robot

--- prediction.py
+++ prediction.py
@@ -42,40 +42,37 @@ OLD MODEL
 pipe = pipeline(task="depth-estimation", model="depth-anything/Depth-Anything-V2-Small-hf")
 
 def forcast_x_min_depth(image, _time=''):
-    start = time.time()
+    # Convert numpy array to PIL
     image = Image.fromarray(image)
+
+    start = time.time()
+
     prediction = pipe(image)["depth"]
-    prediction = torch.from_numpy(np.array(prediction))
+
     end = time.time()
     print("Time of inference:", end - start)
 
-    # # Send image to device    
-    # input_batch = transform(image).to(device)
+    # Convert back PIL image to numpy array
+    prediction = torch.from_numpy(np.array(prediction))
 
-    # with torch.no_grad():
-    #     start = time.time()
-    #     prediction = midas(input_batch)
-
-    #     prediction = torch.nn.functional.interpolate(
-    #         prediction.unsqueeze(1),
-    #         size=image.shape[:2],
-    #         mode="bicubic",
-    #         align_corners=False,
-    #     ).squeeze()
-
-    #     end = time.time()
-    #     print("Time of inference:", end - start)
-
+    # Save predicted image
     cv2.imwrite(f"predictions/pred-{_time}.png", prediction.cpu().numpy())
+    
+    # Normalize image
     pmin = torch.min(prediction)
     pmax = torch.max(prediction)
     prediction = (prediction - pmin) / (pmax - pmin)
-    #cv2.imwrite(f"out/out2222.png", 255*prediction.cpu().numpy())
 
+    # Average pooling to archive a 1x11 vector
     prediction = torch.nn.functional.adaptive_avg_pool2d(prediction.unsqueeze(0), (1,11)).squeeze()
-    # cv2.imwrite(f"out/outchan.png", 255*prediction.cpu().numpy())
-    print(prediction)
-    
+
+    # We divide image into 11 parts and 
+    # extract the index of the part with minimun depth
+    # The maximun angular velocitiy of the robot is 5 rad/s
+    # the robot will rotate as much as the index is far the center (index 5 in this case because our array has lenght 11)
+    # The indexes look like this:
+    # [-5 -4 -3 -2 -1 0 1 2 3 4 5]
+    # The index is the estimate angular velocity of the robot for its next step.
     i = 5 - torch.argmin(prediction)
 
     # Save prediction in file
@@ -85,38 +82,10 @@ def forcast_x_min_depth(image, _time=''):
     return i.item()
 
 
-# def forcast_x_min_depth(depth_map):
-#     z = torch.mean(depth_map, dim=0)
-#     z = z.unsqueeze(0)
-#     nsz = 50
-#     z = torch.nn.functional.adaptive_avg_pool1d(z, nsz).squeeze()
-#     i = torch.argmin(z)
-#     return i, z
-
 def estimate_robot_motion(image, _time=''):
     w = forcast_x_min_depth(image,  _time=_time)
-    return 0.2, w / 3.14
-    # distancia focal f (mm)
-    f = 45
-    cx = image.shape[1] // 2
-    v = np.array([f, cx-xmin])
-    u = np.array([f, 0])
-    
-    dot_product = np.dot(u, v)
-    unorm = np.linalg.norm(u)
-    vnorm = np.linalg.norm(v)
-    
-    # Asegurar que el valor esté en el rango [-1, 1] para evitar errores numéricos
-    cos_theta = dot_product / (unorm * vnorm)
-    cos_theta = np.clip(cos_theta, -1.0, 1.0)
-    
-    # Obtener el ángulo en radianes y convertir a grados
-    angle = np.arccos(cos_theta)
-    print(f'Angulo: -60/{angle}/60')
-    # Calculamos la velocidad angular
-    # angle/max_angle * max_vel
-    w = 5*angle/120
-    
-    return 0.2, w
-    
 
+    # Return velocity and normalized angular velocity
+    return 0.2, w
+
+# q
